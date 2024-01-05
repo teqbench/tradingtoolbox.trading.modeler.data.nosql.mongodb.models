3f03bcf9f924e580aace624fabf1d2502e8f48dd
Ben <ben.visintainer@me.com>
2024-01-04 22:07:41 -0700

    Refactored structure to user src an tests folders to segregate source code from test code. Removed old workflow scripts and added new ones. Updated README.md to have badges.

---
+++ tests/PositionModelDocumentTests.cs
@@ -0,0 +1,10 @@
+﻿namespace TradingToolbox.Trading.Modeler.Data.NoSql.MongoDB.Models.Tests;
+
+[TestClass]
+public class PositionModelDocumentTests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
